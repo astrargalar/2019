769b9e5966cabd762dbdbbb8e8795452aa933f6a
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-07-23 14:01:48 +0200

    Pongo grid a header

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 	<div id="page" class="site">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to content', 'mitema'); ?></a>
 
-		<header id="masthead" class="<?php echo is_singular() && mitema_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
+		<header id="masthead" class="<?php echo is_singular() && mitema_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?> ">
 
 			<div class="site-branding-container">
 				<?php get_template_part('template-parts/header/site', 'branding'); ?>
